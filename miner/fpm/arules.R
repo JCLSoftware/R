74df4a3f6d397ec20be858e915b79c638ca4230d
Jean Leão <jclsoftware@itinganet.com.br>
2018-12-29 09:43:25 -0200

    Update arules.R

--- miner/fpm/arules.R
+++ miner/fpm/arules.R
@@ -17,8 +17,6 @@
 	p<-plot(apr, method = "graph", engine="htmlwidget", measure = "support", shading = "lift", fp)
 	htmlwidgets::saveWidget(p, "arules.html", selfcontained = FALSE)
 	file.rename("arules.html", paste(defaultName, "graph.html", sep='_'))
-	#ATENCAO! Renomeie o arquivo pois será gerado novo arquivo a seguir. Isso é necessário para que a pasta de biblioteca tenha um nome unico e melhore o desempenho do sistema
-
 	p <- plotly_arules(apr)
 	htmlwidgets::saveWidget(p, "arules.html", selfcontained = FALSE)
         file.rename("arules.html", paste(defaultName, "scatter.html", sep='_'))
