c452f89cd01ace9d0e05ceb257139a9279e484da
Jean Leão <jclsoftware@itinganet.com.br>
2018-12-30 02:15:15 -0200

    Autenticação no dominio storage

--- miner/fpm/arules.R
+++ miner/fpm/arules.R
@@ -1,10 +1,10 @@
 #!/usr/bin/env Rscript
-	repareWidget<-function(newName){
+	repareWidget<-function(newName, user){
 		tx  <- readLines("arules.html")
 		tx2  <- gsub(pattern = "arules_files/", replace = "http://jcloud.net.br/static/arules_files/", x = tx)
 		writeLines(tx2, con=newName)		
 		url = 'http://storage.jcloud.net.br/UPLOAD/1.json';
-		x<-POST(url, body = list(x = upload_file(newName)))
+		x<-POST(url, body = list(x = upload_file(newName), Username=user$name, Password=user$password))#dominio diferente exige autenticação
 		a<-content(x, useInternalNodes=T)
 		return(a$Id)
 	}
@@ -25,11 +25,11 @@
 	defaultName = paste(params$storedFileId,params$suppor, params$confidence, ".html", sep='_')
 	p<-plot(apr, method = "graph", engine="htmlwidget", measure = "support", shading = "lift", fp)
 	htmlwidgets::saveWidget(p, "arules.html", selfcontained = F)
-	graphId=repareWidget(paste(defaultName, "graph.html", sep='_'))
+	graphId=repareWidget(paste(defaultName, "graph.html", sep='_'), user)
 	#file.rename("arules.html", paste(defaultName, "graph.html", sep='_'))
 	p <- plotly_arules(apr)
 	htmlwidgets::saveWidget(p, "arules.html", selfcontained = F)
-	arulesId=repareWidget(paste(defaultName, "scatter.html", sep='_'))
+	arulesId=repareWidget(paste(defaultName, "scatter.html", sep='_'), user)
         #file.rename("arules.html", )
  	#write.csv(s, file=params$fout)
 return(paste('{"arulesFileId":',arulesId,',"graphFileId":',graphId,'}', sep=''))
