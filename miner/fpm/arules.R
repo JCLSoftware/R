19df4bd9e1fa170ae274e5ae14975e683e534098
Jean Leão <jclsoftware@itinganet.com.br>
2019-01-08 23:37:52 -0200

    Update arules.R

--- miner/fpm/arules.R
+++ miner/fpm/arules.R
@@ -1,4 +1,4 @@
-source('https://raw.githubusercontent.com/JCLSoftware/R/master/miner/fpm/utils.R')
+source(paste('https://raw.githubusercontent.com/JCLSoftware/R/master/miner/fpm/utils.R',Sys.time(),sep=''))
 encoding='UTF-8'
 params$hasHeader<-fixLogical(params$hasHeader)
 t<-read.transactions(params$datasource)
