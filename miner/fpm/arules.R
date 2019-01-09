fc835a8e5c31e795037fb18c2c41aecaf6ccec38
Jean Leão <jclsoftware@itinganet.com.br>
2019-01-08 23:33:57 -0200

    Update arules.R

--- miner/fpm/arules.R
+++ miner/fpm/arules.R
@@ -3,4 +3,5 @@ encoding='UTF-8'
 params$hasHeader<-fixLogical(params$hasHeader)
 t<-read.transactions(params$datasource)
 apr<-apriori(t, parameter = list(supp = as.numeric(params$support), conf = as.numeric(params$confidence)))
-writeOutputs(params)
+result<-writeOutputs(params)
+return(result)
