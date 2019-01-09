ca4cbc7289fac51f4efd3bc80b98ed56a35e4fd5
Jean Leão <jclsoftware@itinganet.com.br>
2019-01-08 23:34:25 -0200

    Update tabularARules.R

--- miner/fpm/tabularARules.R
+++ miner/fpm/tabularARules.R
@@ -16,4 +16,5 @@ write.table(frules, x = data, sep = ';')
 fp <- read.transactions(frules, sep = ';')
 data<-fp;
 apr<-apriori(fp, parameter = list(supp = as.numeric(params$support), conf = as.numeric(params$confidence)))
-writeOutputs(params)
+result<-writeOutputs(params)
+return(result)
