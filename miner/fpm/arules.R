48abe38e4767dfeee082536e29a72fadfc788fbd
Jean Leão <jclsoftware@itinganet.com.br>
2019-01-08 23:28:28 -0200

    Update arules.R

--- miner/fpm/arules.R
+++ miner/fpm/arules.R
@@ -1,31 +1,6 @@
-#!/usr/bin/env Rscript
-	repareWidget<-function(newName, user){
-		tx  <- readLines("arules.html")
-		tx2  <- gsub(pattern = "arules_files/", replace = "http://jcloud.net.br/static/arules_files/", x = tx)
-		writeLines(tx2, con=newName)		
-		url = 'http://storage.jcloud.net.br/UPLOAD/1.json';
-		x<-POST(url, body = list(x = upload_file(newName), Username=user$name, Password=user$password))#dominio diferente exige autenticação
-		a<-content(x, useInternalNodes=T)
-		return(a$Id)
-	}
-	encoding='UTF-8'
-	#Fix missing values (TODO: use function fixLogical from https://raw.githubusercontent.com/JCLSoftware/R/master/miner/utils.R, already loaded)
-	if(length(params$hasHeader) == 0){
-		params$hasHeader=F
-	}else { params$hasHeader=as.logical(params$hasHeader)}
-
-	t = read.table(params$datasource, sep=params$sep, header = params$hasHeader, row.names=NULL, comment.char="", stringsAsFactors=TRUE, encoding = encoding)	
-	data<-as.matrix(t)
-	#data[is.na(data[,2]),2]='Outros' #Remove NAs das cidades
-	for(i in 1:nrow(data)){
-	  for(j in 1:ncol(data)){
-	    data[i, j]<-paste(j, data[i, j])
-	  }
-	}
-	frules<-paste(params$fout, '.rules', sep='')
-	write.table(frules, x = data, sep = ';')
-	fp <- read.transactions(frules, sep = ';')
-	data<-fp;
-	apr<-apriori(fp, parameter = list(supp = as.numeric(params$support), conf = as.numeric(params$confidence)))
-	defaultName = paste(params$storedFileId,params$suppor, params$confidence, ".html", sep='_')
-	source('https://raw.githubusercontent.com/JCLSoftware/R/master/miner/fpm/writeOutputs.R')
+source('https://raw.githubusercontent.com/JCLSoftware/R/master/miner/fpm/utils.R')
+encoding='UTF-8'
+params$hasHeader<-fixLogical(params$hasHeader)
+t<-read.transactions(params$datasource)
+apr<-apriori(t, parameter = list(supp = as.numeric(params$support), conf = as.numeric(params$confidence)))
+writeOutputs(params)
